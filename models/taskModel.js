1f603b20cedbd1670de18d90cf8ece598486d29d
rohit <rkgupta9534@gmail.com>
2023-10-01 02:30:46 +0530

    first commit

---
+++ models/taskModel.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+const taskInstance = mongoose.Schema({
+    title:{type:String, required:true},
+    description:{type:String, required:true},
+    priority:{type:String},
+    userId:{type:String, required:true},
+    completed:{type:Boolean, required:true}
+}, {timestamps:true});
+
+const taskModel = mongoose.model("Task", taskInstance);
+export default taskModel;
